bc20bf8869a68ca14dce19113c2ee90e65378464
Arshad Ullah <mdarshadullah835@gmail.com>
2025-01-10 19:11:06 +0600

    product card done

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,33 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import Title from "../title/Title";
 import Heading from "../heading/Heading";
 import Pragraph from "../pragraph/Pragraph";
-import Button from "../button/Button";
-
+import img from "../../assets/vegitable.jpg";
+import ProductCard from "../productCard/ProductCard";
+const data = [
+  {
+    id: 1,
+    img: img,
+    title: " Lorem, ipsum dolor  ",
+    price: "10",
+  },
+  {
+    id: 2,
+    img: img,
+    title: " Lorem, ipsum dolor  ",
+    price: "10",
+  },
+  {
+    id: 3,
+    img: img,
+    title: " Lorem, ipsum dolor  ",
+    price: "10",
+  },
+  {
+    id: 4,
+    img: img,
+    title: " Lorem, ipsum dolor  ",
+    price: "10",
+  },
+];
 const Product = () => {
+  const [active, setActive] = useState(null);
+  console.log(active);
   const buttonData = [
     { id: 1, Name: "All" },
     { id: 2, Name: "Fruits" },
     { id: 3, Name: "Vegetables" },
     { id: 4, Name: "Salad" },
   ];
+
   return (
-    <div className="py-10">
-      <div className="flex justify-center items-center ">
+    <div className="py-10 px-28">
+      <div className="flex justify-center items-center">
         <div className="text-center">
-          <Title props={"Our Products"} />
-          <Heading props={"Our Fresh Products"} />
-          <Pragraph
-            props={
-              "We pride ourselves on offering a wide variety of fresh and flavorful fruits, vegetables, and salad ingredients."
-            }
-          />
+          <Title props="Our Products" />
+          <Heading props="Our Fresh Products" />
+          <Pragraph props="We pride ourselves on offering a wide variety of fresh and flavorful fruits, vegetables, and salad ingredients." />
         </div>
       </div>
-      <div className="flex justify-center gap-2">
-        {buttonData?.map((obj) => {
-          return <Button key={obj.id} props={obj?.Name} />;
-        })}
+      <div>
+        <div className="flex justify-center gap-2 mt-4">
+          {buttonData?.map((obj) => (
+            <button
+              className={`text-xl   text-center rounded-md py-2 px-3 cursor-pointer ${
+                active === obj?.id
+                  ? "  text-white bg-[#749B3F]"
+                  : "  bg-white text-[#749B3F] border border-[#749B3F]"
+              }`}
+              key={obj?.id}
+              onClick={() => setActive(obj?.id)}
+            >
+              {obj?.Name}
+            </button>
+          ))}
+        </div>
+        <div className="grid my-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+          {data.map((item) => {
+            return (
+              <div key={item?.id}>
+                <ProductCard
+                  title={item?.title}
+                  img={item?.img}
+                  price={item?.price}
+                />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import Button from "../button/Button";
 
 const ProductCard = ({ className, img, title, price }) => {
   return (
-    <div className={`${className}`}>
-      <img src={img} alt="" />
-      <span>{title}</span>
-      <span>{price}</span>
+    <div className={`${className} rounded-md shadow`}>
+      <img className="w-full h-full mb-4 rounded-md" src={img} alt="" />
+      <div className="p-3">
+        <div className="text-center ">
+          <span className="mb-4 text-lg text-secondary font-medium">
+            {title}
+          </span>{" "}
+          <br />
+          <span className="text-lg text-secondary">${price}</span>
+        </div>
+        <Button className={"my-3"} props={"Add To Cart"} />
+      </div>
     </div>
   );
 };
